122514d24116faf9521ebf9f28f735cae041c4c1
Rodrigo Rufino Elias <rodrigo.rufino.elias@gmail.com>
2021-11-01 18:20:04 -0300

    Curso VueJS Completo

---
+++ alurapic/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="corpo">
+
+    <meu-menu :rotas="routes"></meu-menu>
+
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition>
+  </div>
+</template>
+
+<script>
+import { routes } from './routes';
+import Menu from './components/shared/menu/Menu.vue';
+
+export default {
+  name: 'app',
+  components: {
+    'meu-menu': Menu
+  },
+  data() {
+    return {
+      routes: routes.filter(route => route.menu)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    width: 96%;
+    margin: 0 auto;
+  }
+  .pagina-enter-active, .pagina-leave-active {
+    transition: opacity .3s
+  }
+  .pagina-enter, .pagina-leave-active {
+    opacity: 0
+  }
+</style>
---
+++ alurapic/src/components/shared/botao/Botao.vue
@@ -0,0 +1,56 @@
+<template>
+  <button class="botao" :class="estiloDoBotao" :type="tipo" @click="disparaAcao()">
+    {{ rotulo }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    tipo: {
+      required: true,
+      type: String
+    },
+
+    rotulo: {
+      required: true,
+      type: String
+    },
+
+    confirmacao: {
+      required: false,
+      default: false,
+      type: Boolean
+    },
+
+    estilo: {
+      required: false,
+      default: 'padrao',
+      type: String
+    }
+  },
+   methods: {
+    disparaAcao() {
+      if(this.confirmacao) {
+        if(confirm('Confirma operacao?')) {
+          this.$emit('botaoAtivado');
+        }
+        return;
+      }
+      this.$emit('botaoAtivado');
+    }
+   },
+   computed: {
+     estiloDoBotao() {
+      if(this.estilo == 'padrao') return 'botao-padrao';
+      if(this.estilo == 'perigo') return 'botao-perigo';
+     }
+   }
+}
+</script>
+
+<style lang="sass" scoped>
+
+  @import './Botao.scss';
+
+</style>
